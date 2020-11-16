5474772e63d0c58aa27186f1e4212523fbeb36e4
Akshay Jain <jiak@amazon.com>
2020-11-15 19:54:59 -0700

    Fix font issue

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -25,6 +25,22 @@ export class Landing extends Component {
     render() {
         return (
             <div>
+                <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+                <link rel="stylesheet" type="text/css" href="css/style.css" />
+                <link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css" />
+                <link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css" />
+                <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css" />
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet" />
+                <script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+                <script type="text/javascript" src="js/bootstrap.js"></script>
+                <script type="text/javascript" src="js/SmoothScroll.js"></script>
+                <script type="text/javascript" src="js/nivo-lightbox.js"></script>
+                <script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+                <script type="text/javascript" src="js/contact_me.js"></script>
+                <script type="text/javascript" src="js/main.js"></script>
+
                 <Navigation />
                 <Header data={this.state.landingPageData.Header} />
                 <Features data={this.state.landingPageData.Features} />
